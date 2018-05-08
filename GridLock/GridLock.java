f254711875b9f54688c91d651e4074058367c237
Jason Do <Jason.Do1@hotmail.com>
2018-05-08 12:25:50 +1000

    Added GUI files

---
+++ GridLock/GridLock.java
@@ -0,0 +1,10 @@
+import javax.swing.SwingUtilities;
+
+public class GridLock {
+
+	public static void main(String[] args) {
+		GridLockFrame gridLock = new GridLockFrame();
+		SwingUtilities.invokeLater(gridLock);
+	}
+
+}
