c3d78667abcced33dc5715064f1a2fd2f3fd37bb
darshanraju <darshan.raju99@gmail.com>
2018-05-15 12:26:07 +1000

    Add files via upload

--- GridLock/Menu.java
+++ GridLock/Menu.java
@@ -1,85 +1,67 @@
-import java.awt.*;
-import java.awt.BasicStroke;
-import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.GridLayout;
-import java.awt.Point;
-import java.awt.Rectangle;
-import java.awt.event.MouseListener;
-import java.awt.Robot;
-import java.awt.Shape;
-import java.awt.Stroke;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import javax.imageio.*;
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-
+import java.awt.Dimension;
+import javax.swing.JFrame;
 
 public class Menu extends JPanel {
-    public Rectangle playButton = new Rectangle(235, 150, 150, 50);
-    public Rectangle exitButton = new Rectangle(235, 250, 150, 50);
-    public Rectangle helpButton = new Rectangle(235, 350, 150, 50);
+	JButton start = new JButton();
+	JButton help = new JButton();
+	JButton quit = new JButton();
+	GridLockFrame game = new GridLockFrame();
 
+	public Menu() {
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		add(Box.createVerticalStrut(160));
+		start.setAlignmentX(CENTER_ALIGNMENT);
+		start.setPreferredSize(new Dimension(100, 20));
+		ImageIcon startIcon = new ImageIcon(Menu.class.getResource("button_play (1).png"));
+		start.setIcon(startIcon);
+		start.addActionListener(new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				game.runGame();
+			}
+		});
+		add(start);
 
+		add(Box.createVerticalStrut(75));
+		help.setAlignmentX(CENTER_ALIGNMENT);
+		help.setPreferredSize(new Dimension(100, 20));
+		ImageIcon helpIcon = new
+		ImageIcon(Menu.class.getResource("button_help.png"));
+		help.setIcon(helpIcon);
+		help.addActionListener(new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				game.helpPage();
+			}
+		});
 
-    @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        JLabel backgroundImage;
-        ImageIcon menuIcon = new ImageIcon(this.getClass().getResource("Menu Background.png"));
-        backgroundImage = new JLabel("",menuIcon,JLabel.CENTER);
-        backgroundImage.setBounds(0,0,600,600);
-        add(backgroundImage);
-        //g.drawImage(menuIcon, 0, 0, getWidth(), getHeight(), this); // draw the image
-        //this.setBackground(Color.BLACK);
-        render(g);
-    }
+		ImageIcon icon = new ImageIcon(Menu.class.getResource("button_exit.png"));
+		quit.setIcon(icon);
+		add(help);
+		add(Box.createVerticalStrut(75));
+		quit.setAlignmentX(CENTER_ALIGNMENT);
+		start.setPreferredSize(new Dimension(100, 20));
+		// ImageIcon quitIcon = new
+		// ImageIcon(Menu.class.getResource("button_exit.png"));
+		// quit.setIcon(quitIcon);
+		quit.addActionListener(new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		add(quit);
+	}
 
-    public void render(Graphics g){
-        Graphics2D g2d = (Graphics2D) g;
-
-        Font fnt0 = new Font("arial", Font.BOLD,50);
-        g.setFont(fnt0);
-        g.setColor(Color.white);
-        g.drawString("GridLock", 200,100);
-
-        Font fnt1 = new Font("Arial", Font.BOLD, 30);
-        g.setFont(fnt1);
-        g.drawString("Play", 280, 185);
-        g.drawString("Exit", 280, 285);
-        g.drawString("Help", 280, 385);
-        g2d.draw(playButton);
-        g2d.draw(exitButton);
-        g2d.draw(helpButton);
-    }
-
-    public void mousePressed(MouseEvent e) {
-        int x = e.getX();
-        int y = e.getY();
-
-        if(x>235 && x<385){
-            if(y>150 && y<200){
-                //Click on playButton
-                GridLockFrame.State = GridLockFrame.State.GAME;
-            }
-            if(y>150 && y<200){
-                //Click on exitButton
-            }
-            if(y>150 && y<200){
-                //Click on helpButton
-            }
-        }
-    }
-
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawImage(new ImageIcon(Menu.class.getResource("MenuComplete.png")).getImage(), 0, 0, 640, 658, this);
+	}
 }
