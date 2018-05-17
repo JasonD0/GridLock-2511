a6898749ab2b722ecc7163db761537b4bdb6fa73
darshanraju <darshan.raju99@gmail.com>
2018-05-17 11:17:25 +1000

    Add files via upload

--- GridLock/Help.java
+++ GridLock/Help.java
@@ -1,41 +1,58 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
-
-import javax.swing.AbstractAction;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JPanel;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import javax.swing.*;
+import java.awt.Dimension;
 
 public class Help extends JPanel {
-	GridLockFrame game;
-	JButton Return = new JButton();
-	
-	public Help(GridLockFrame game) {
-		this.game = game;
-		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		
-		add(Box.createVerticalStrut(160));
-		Return.setAlignmentX(CENTER_ALIGNMENT);
-		Return.setPreferredSize(new Dimension(100, 20));
-		ImageIcon startIcon = new ImageIcon(Menu.class.getResource("button_play (1).png"));
-		Return.setIcon(startIcon);
-		
-		Return.addActionListener(new AbstractAction() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				game.mainMenu();
-			}
-		});
-		add(Return);
-	}
+    GridLockFrame game;
+    JTextPane helpText = new JTextPane();
+    JButton Return = new JButton();
 
-	@Override
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-        setBackground(Color.blue);
-	}
+    public Help(GridLockFrame game) {
+        this.game = game;
+        setLayout(null);
+
+        add(Box.createVerticalStrut(160));
+        Return.setPreferredSize(new Dimension(100, 20));
+        ImageIcon startIcon = new ImageIcon(Menu.class.getResource("button_return.png"));
+        Return.setIcon(startIcon);
+        Return.setBounds(275,500, 108,43);
+
+        Return.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //LINK TO MAIN MENU
+            }
+        });
+        add(Return, BorderLayout.SOUTH);
+
+        add(Box.createVerticalStrut(160));
+        String s =
+                "The objective of the game is to get the red car out of the game                                   " +
+                        "Vertical cars only move vertically!  " +
+                        "Horizontal cars only move horizontally!  " +
+                        "Drag these cars around the grid to give way for the red car to reach the exit";
+        helpText.setText(s);
+        helpText.setFont(new Font("Serif", Font.HANGING_BASELINE, 28 ));
+        helpText.setBounds(10,40,500,500);
+        helpText.setOpaque(false);
+        helpText.setBorder(BorderFactory.createEmptyBorder());
+        helpText.setBackground(new Color(0,0,0,0));
+        helpText.setLocation(80,80);
+        add(helpText);
+        setOpaque(false);
+    }
+
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(new ImageIcon(Menu.class.getResource("helpBackground.png")).getImage(), 0, 0, 640, 658, this);
+    }
 }
