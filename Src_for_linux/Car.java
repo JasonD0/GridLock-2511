ec30926f5cf7bf2c2d843bd690a9b2ade0ad5538
puapao <puapao123@gmail.com>
2018-05-25 16:47:06 +1000

    java doc for backend

--- Src_for_linux/Car.java
+++ Src_for_linux/Car.java
@@ -8,6 +8,17 @@ public class Car {
 	private String color;
 	private boolean red;
 	
+	/**
+	 * Constructor for car
+	 * @param x
+	 * @param y
+	 * @param length
+	 * @param height
+	 * @param orientation
+	 * @param size
+	 * @param id
+	 * @param red
+	 */
 	public Car(int x, int y, int length, int height, String orientation, int size, int id, boolean red) {
 		this.x = x;
 		this.y = y;
@@ -18,51 +29,81 @@ public class Car {
 		this.id = id;
 		this.red = red;
 	}
-	
+	/**
+	 * 
+	 * @return color of the car
+	 */
 	public String getColor() {
 		return this.color;
 	}
-	
+	/**
+	 * check whether the car is red or not
+	 * @return 
+	 */
 	public boolean isRed() {
 		return this.red;
 	}
-	
+	/**
+	 * @return get the ID of the car
+	 */
 	public int getId() {
 		return this.id;
 	}
-	
+	/**
+	 * changing the x coordinate
+	 * @param x
+	 */
 	public void setX(int x) { 
 		this.x = x; 
 	}
-	
+	/**
+	 * changing the y coordinate
+	 * @param y
+	 */
 	public void setY(int y) { 
 		this.y = y; 
 	}
-	
+	/**
+	 * @return get x coordinate of the car
+	 */
 	public int getX() { 
 		return this.x; 
 	}
-	
+	/**
+	 * @return get y coordinate of the car
+	 */
 	public int getY() { 
 		return this.y; 
 	}
-	
+	/**
+	 * @return get the size of the car
+	 */
 	public int getSize() {
 		return this.size;
 	}
-	
+	/**
+	 * @return get the height of the car
+	 */
 	public int getHeight() {
 		return this.height;
 	}
-	
+	/**
+	 * @return get the length of the car
+	 */
 	public int getLength() {
 		return this.length;
 	}
-	
+	/**
+	 * 
+	 * @return string telling the direction of where the vehicle is facing
+	 */
 	public String orientation() {
 		return this.orientation;
 	}
-	
+	/**
+	 * @param point
+	 * @return boolean
+	 */
 	public boolean contains(Point point) {
 		if (point.x < this.x || point.y < this.y) return false;
 		if (point.x > this.x + length || point.y > this.y + height) return false;
